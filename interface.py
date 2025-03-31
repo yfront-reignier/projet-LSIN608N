24fd695a24e40361e0be4df4ff1bd3ba665a0fec
Anissa Kourban Houssen Fassy <anissa.kourban-houssen-fassy@ens.uvsq.fr>
2025-03-31 19:59:57 +0200

    continue d'avancer sur tkinter en POO

--- interface.py
+++ interface.py
@@ -1,24 +1,37 @@
 import tkinter as tk
 import numpy as np
 
-
-
 class Interface(tk.Tk):
     def __init__(self): 
-        tk.Tk.__init__(self)
-        self.create_config()
+        super().__init__()
 
-    def create_config(self):
-        self.geometry('720x720')
+        self.wm_title("Othello")
+        self.geometry("720x640")
         self.config = self.configure(bg = "#e6dbbe")
-        self.bouton = tk.Button(self, text="Quitter", command=self.quit)
-        self.bouton.pack()
+
+        self.mainmenu = MainMenu(self)
 
 
-    
-    def show_grid(matrix):
-        matrix = np.array()
+class MainMenu(tk.Frame):
+    def __init__(self, parent):
+        super().__init__(parent)
 
+        self.parent = parent
+
+        self.pack()
+
+        self.button_start = tk.Button(self, text = "Jouer", command = self.start_game())
+        self.button_start.pack()
+
+        self.button_quit = tk.Button(self, text = "Quitter", command = self.quit())
+        self.button_quit.pack()
+
+    def start_game(self):
+        self.game = Game(self)
+
+class Game(tk.Frame):
+    def __init__(self, parent):
+        super().__init__(parent)
 
 
 if __name__ == '__main__': 
