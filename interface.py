20a1ddaaaf15b4dd296d46bdd2269c6710f1c735
heyu12138 <he.yu@ens.uvsq.fr>
2025-04-04 11:56:34 +0200

    ptn ça fonctionne

--- interface.py
+++ interface.py
@@ -8,15 +8,14 @@ class Interface(tk.Tk):
         super().__init__()
 
         self.wm_title("Othello")
-        self.geometry("720x640")
+        self.geometry("500x500")
         self.config = self.configure(bg = "#e6dbbe")
 
         self.mainmenu = MainMenu(self)
-        self.mainmenu.grid()
-
 
 class MainMenu(tk.Frame):
     def __init__(self, parent):
+        self.parent=parent
         super().__init__(parent)
         
 
@@ -32,7 +31,7 @@ class MainMenu(tk.Frame):
         self.cnv.grid(row=0, column=0, rowspan=4, columnspan=3)
         self.cnv.create_image(0, 0, image=self.fond_tk, anchor="nw")
         
-        self.jcj_button = tk.Button(parent, text="Joueur vs Joueur", padx=20, pady=10, font=("Arial", 10))
+        self.jcj_button = tk.Button(parent, text="Joueur vs Joueur", padx=20, pady=10, font=("Arial", 10), command=self.start_game)
         self.jcj_button.grid(row=1,column=0,columnspan=2)
         
         self.jcia_button = tk.Button(parent,text="Joueur vs IA",padx=20, pady=10, font=("Arial", 10))
@@ -50,15 +49,32 @@ class MainMenu(tk.Frame):
         parent.grid_columnconfigure(2, weight=1)
 
     def start_game(self):
-        self.game = Game(self)
+        self.game = Game(self.parent)
+        
 
 class Game(tk.Frame):
     def __init__(self, parent):
         super().__init__(parent)
+        self.yellow_circles=[]
+        self.canvas_pions={}
+
+        self.plateau=tk.Canvas(parent,width=800,height=800,background='green')
+        self.createGrid(8)
+        
+    def createGrid(self, dim_matrix):
+        self.plateau.grid(row=0,column=0,rowspan=4,columnspan=4)
+        self.plateau.create_rectangle(0,0,800,800,fill="green")
+
+        for i in range(dim_matrix):
+            for j in range(dim_matrix):
+                x0 = i*100
+                y0 = j*100
+                x1 = (i+1)*100
+                y1 = (j+1)*100
+                self.plateau.create_rectangle(x0,y0,x1,y1,fill="green",outline="black")
+
 
 
 if __name__ == '__main__': 
     vizualiser = Interface()
     vizualiser.mainloop()
-    v2 = MainMenu()
-    v2.mainloop()
