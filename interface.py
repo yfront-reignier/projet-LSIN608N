91a0f0c139de008eeb2b6ac7ad73f11244c06422
heyu12138 <he.yu@ens.uvsq.fr>
2025-04-11 09:27:50 +0200

    interface

--- interface.py
+++ interface.py
@@ -12,8 +12,10 @@ class Interface(tk.Tk):
         super().__init__()
 
         self.wm_title("Othello")
-        self.geometry("1000x1000")
+        self.geometry(f"{self.winfo_screenwidth()}x{self.winfo_screenheight()}")
         self.config = self.configure(bg = "#e6dbbe")
+        self.width=self.winfo_screenwidth()
+        self.height=self.winfo_screenheight()
 
         self.mainmenu = MainMenu(self)
 
@@ -21,7 +23,7 @@ class MainMenu(tk.Frame):
     def __init__(self, parent):
         self.parent=parent
         super().__init__(parent)
-        
+
         self.label = tk.Label(parent,text = "Othello \n⚫VS⚪ ",bg="#e6dbbe", justify=tk.CENTER)
         self.label.grid(row =0, column=0,rowspan=1, columnspan=4)
         self.label.config(font=("ArcadeClassic",100 ))
@@ -35,22 +37,22 @@ class MainMenu(tk.Frame):
         # self.cnv.create_image(0, 0, image=self.fond_tk, anchor="nw")
         
         self.jcj_button = tk.Button(parent, text="Joueur vs Joueur", padx=20, pady=10, bd=5, font=("Arial", 14,"bold"),
-                                    bg="#228B22",fg="white",activebackground="#008000",activeforeground="white",
+                                    bg="#228B22",fg="white",
                                     relief="raised",  command=lambda: self.start_game('jvsj'))
         self.jcj_button.grid(row=1,column=0,columnspan=2)
         
         self.jcia_button = tk.Button(parent,text="Joueur vs IA",padx=20, pady=10, bd=5, font=("Arial", 14,"bold"),
-                                    bg="#228B22",fg="white",activebackground="#008000",activeforeground="white",
+                                    bg="#228B22",fg="white",
                                     relief="raised",command=lambda: self.start_game('jvsia'))
         self.jcia_button.grid(row=1,column=1,columnspan=4)
 
         self.button_regle = tk.Button(parent, text = "Règle du Jeu", padx=20, pady=10, bd=5, font=("Arial", 14,"bold"),
-                                    bg="#696969",fg="white",activebackground="#000000",activeforeground="white",
+                                    bg="#696969",fg="white",
                                     relief="raised", command = self.regle)
         self.button_regle.grid(row=2,column=1,columnspan=4)
         
         self.button_quit = tk.Button(parent, text = "Quitter", padx=20, pady=10, bd=5, font=("Arial", 14,"bold"),
-                                    bg="#696969",fg="white",activebackground="#000000",activeforeground="white",
+                                    bg="#696969",fg="white",
                                     relief="raised", command = parent.quit)
         self.button_quit.grid(row=2, column=0, columnspan=2)
 
@@ -97,7 +99,7 @@ class RegleDuJeu(tk.Frame):
         self.ecrit.grid(row=0,column=1)
 
         self.replay_button = tk.Button(parent, text="Retourner au Menu",padx=20, pady=10, bd=5, font=("Arial", 14,"bold"),
-                                    bg="#696969",fg="white",activebackground="#000000",activeforeground="white",
+                                    bg="#696969",fg="white",
                                     relief="raised",  command = self.replay_game)
         self.replay_button.grid(row=1, column=1)
 
@@ -133,22 +135,37 @@ class Game(tk.Frame):
         parent.grid_columnconfigure(0, weight=1)
         parent.grid_columnconfigure(1, weight=1)
         parent.grid_columnconfigure(2, weight=1)
-
-        self.hint=tk.Button(parent,text='Indices-Toute la partie',font=("Helvetica", 16, "bold"), fg="black", bg="#f0e6d6",command=self.show_hints_all_game)
-        self.hint.grid(row=4,column=0)
         
-        self.hint=tk.Button(parent,text='Indices-Un tour',font=("Helvetica", 16, "bold"), fg="black", bg="#f0e6d6",command=self.show_hints)
-        self.hint.grid(row=3,column=0)
+        self.bg=tk.Canvas(parent,width=250,height=750,bg="#fdfbf9")
+        self.bg.place(x=20,y=25)
 
-        self.label = tk.Label(parent, text="⚫Pions noir: "+str(self.othellier._countPaws()[0]),font=("Helvetica", 16, "bold"), fg="black", bg="#f0e6d6")
-        self.label.grid(row=2,column=0)
-
-        self.label2 = tk.Label(parent, text="⚪Pions blanc: "+str(self.othellier._countPaws()[0]),font=("Helvetica", 16, "bold"), fg="black", bg="#f0e6d6")
-        self.label2.grid(row=1,column=0)
+        self.bg2=tk.Canvas(parent,width=250,height=750,bg="#a67d3c")
+        self.bg2.place(x=1250, y=25)
 
         self.label3 = tk.Label(parent, text="Tour du joueur: Noir",font=("Helvetica", 16, "bold"), fg="black", bg="#f0e6d6")
-        self.label3.grid(row=0,column=0)
+        self.label3.place(x=1285,y=550)
+
+        self.label2 = tk.Label(parent, text="⚪Pions blanc: ", font=("Helvetica", 16, "bold"), fg="black", bg="#f0e6d6")
+        self.label2.place(x=65,y=28)
+
+        self.score_white = tk.Label(parent, text= str(self.othellier._countPaws()[1]), font=("Helvetica", 36, "bold"), fg="black", bg="#fdfbf9")
+        self.score_white.place(x=100,y=300)
+
+        self.label = tk.Label(parent, text="⚫Pions noir: ",font=("Helvetica", 16, "bold"), fg="black", bg="#f0e6d6")
+        self.label.place(x=1295,y=28)
+
+        self.score_black = tk.Label(parent, text= str(self.othellier._countPaws()[0]), font=("Helvetica", 36, "bold"), fg="black", bg="#a67d3c")
+        self.score_black.place(x=1340,y=300)
         
+        self.hint=tk.Button(parent,text='Indices-Un tour',padx=20, pady=10, bd=5, font=("Helvetica", 14,"bold"),
+                                    bg="#f0e6d6",fg="black",relief="raised", command=self.show_hints)
+        
+        self.hint.place(x=35,y=600)
+
+        self.all_hints=tk.Button(parent,text='Indices-Toute \n la partie',padx=20, pady=10, bd=5, font=("Helvetica", 14,"bold"),
+                                    bg="#f0e6d6",fg="black",relief="raised", command=self.show_hints_all_game)
+        self.all_hints.place(x=1295,y=600)
+
         
 
         self.createGrid(self.othellier.GetMatrix())
@@ -176,7 +193,7 @@ class Game(tk.Frame):
 
     def createGrid(self, matrix):
         dim_matrix = matrix.shape[0]
-        self.plateau.grid(row=0,column=1,rowspan=6,columnspan=6)
+        self.plateau.place(x=350,y=0)
         self.plateau.create_rectangle(0,0,800,800,fill="green")
 
         for i in range(dim_matrix):
@@ -204,34 +221,48 @@ class Game(tk.Frame):
 
           
     def maj_score(self):
-        self.label.config(text="Pions noir: "+str(self.othellier._countPaws()[0]))
-        self.label2.config(text="Pions blanc: "+str(self.othellier._countPaws()[1]))
+        self.score_black.config(text=str(self.othellier._countPaws()[0]))
+        self.score_white.config(text=str(self.othellier._countPaws()[1]))
         self.label3.config(text="Tour du joueur: "+self.color[self.othellier.GetPlayer()])
 
     def win_lose(self,parent):
             if self.othellier.winner() == 0:
+                self.bg.destroy()
+                self.bg2.destroy()
+                self.score_black.destroy()
+                self.score_white.destroy()
                 self.label.destroy()
                 self.label2.destroy()
                 self.label3.destroy()
                 self.plateau.destroy()
-                self.label = tk.Label(parent, text="Blanc gagne",font=("Arial", 20))
-                self.label.grid(row=1,column=0,columnspan=4)
+                self.hint.destroy()
+                self.all_hints.destroy()
+                text="Joueur blanc gagne"
             elif self.othellier.winner() == 1:
+                self.bg.destroy()
+                self.bg2.destroy()
+                self.score_black.destroy()
+                self.score_white.destroy()
                 self.label.destroy()
                 self.label2.destroy()
                 self.label3.destroy()
                 self.plateau.destroy()
-                self.label = tk.Label(parent, text="Noir gagne",font=("Arial", 20))
-                self.label.grid(row=1,column=0,columnspan=4)
+                self.hint.destroy()
+                self.all_hints.destroy()
+                text="Joueur noir gagne"
             elif self.othellier.winner() == None:
+                self.bg.destroy()
+                self.bg2.destroy()
+                self.score_black.destroy()
+                self.score_white.destroy()
                 self.label.destroy()
                 self.label2.destroy()
                 self.label3.destroy()
                 self.plateau.destroy()
-                self.label = tk.Label(parent, text="Egalité",font=("Arial", 20))
-                self.label.grid(row=1,column=0,columnspan=4)
-
-            GameOver(self.parent)
+                self.hint.destroy()
+                self.hint.destroy()
+                text="Match nul"
+            GameOver(self.parent,text)
 
     def click(self,event):
         self.delete_hints() 
@@ -242,22 +273,30 @@ class Game(tk.Frame):
         time.sleep(0.5)  
         if self.othellier.game_over():
             self.win_lose(self.parent)
+
+    # def show_bg(self):
+
         
             
 
 class GameOver(tk.Frame):
-    def __init__(self, parent):
+    def __init__(self, parent,text):
         super().__init__(parent)
         self.parent=parent
 
-        self.label = tk.Label(parent, text="Game Over",font=("Arial", 20))
-        self.label.grid(row=1,column=0,columnspan=4)
+        self.label = tk.Label(parent, text="Game Over" ,font=("ArcadeClassic", 70),bg="#e6dbbe", justify=tk.CENTER)
+        self.label.grid(row=0,column=0,columnspan=4)
+
+        self.label2 = tk.Label(parent, text=text,font=("Helvetica", 30),bg="#e6dbbe", justify=tk.CENTER)
+        self.label2.grid(row=1,column=0,columnspan=4)
 
 
-        self.button_quit = tk.Button(parent, text = "Quitter", padx=20, pady=10, command = parent.quit)
+        self.button_quit = tk.Button(parent, text = "Quitter", padx=20, pady=10, bd=5, font=("Helvetica", 14,"bold"),
+                                    bg="#f0e6d6",fg="black",relief="raised",command = parent.quit)
         self.button_quit.grid(row=2, column=0, columnspan=2) 
 
-        self.replay_button = tk.Button(parent, text="Rejouer",padx=20, pady=10, command = self.replay_game)
+        self.replay_button = tk.Button(parent, text="Rejouer",padx=20, pady=10, bd=5, font=("Helvetica", 14,"bold"),
+                                    bg="#f0e6d6",fg="black",relief="raised",command = self.replay_game)
         self.replay_button.grid(row=2, column=1, columnspan=4)
 
         parent.grid_rowconfigure(0, weight=1)
@@ -269,6 +308,7 @@ class GameOver(tk.Frame):
 
     def replay_game(self):
         self.label.destroy()
+        self.label2.destroy()
         self.button_quit.destroy()
         self.replay_button.destroy()
         MainMenu(self.parent)
